0c449e954c142e801bc50104fc0058c15b89fb10
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 15:02:34 +0800

    Add listen function

--- myApp.js
+++ myApp.js
@@ -7,7 +7,10 @@ app.get("/", (req, res) => {
     res.send("Hello Express");
 });
 
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}...`);
+});
 
 
 
