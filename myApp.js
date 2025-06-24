83367af6aadc8d09d45c1034858538fab595dae7
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 16:55:26 +0800

    add next()

--- myApp.js
+++ myApp.js
@@ -31,6 +31,8 @@ app.get("/json", (req, res) => {
 
 app.get("/now", (req, res, next) => {
     req.time = new Date().toString();
+
+    next();
 }, (req, res) => {
     res.json({ time: req.time });
 })
