fd05c9699f87155ea68648b1fdb7bb08195c1fba
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 15:59:58 +0800

    fix static express

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 const stylePath = __dirname = "/public/style.css";
 const indexPath = __dirname + "/views/index.html";
 
-app.use("/", express.static(stylePath));
+app.use(express.static(stylePath));
 
 app.get("/", (req, res) => {
     res.sendFile(indexPath);
