0009fa5f759874b52a14b47cf911b7a22419fe5f
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 16:01:40 +0800

    Fix static path

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ console.log("Hello World");
 const stylePath = __dirname = "/public/style.css";
 const indexPath = __dirname + "/views/index.html";
 
-app.use(express.static(stylePath));
+app.use(express.static("/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(indexPath);
