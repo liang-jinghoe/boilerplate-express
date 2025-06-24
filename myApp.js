c26fdb4ccbc102c1b8eddf9bd1cfddf7edbbc5a5
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 15:08:26 +0800

    Change port

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(indexPath);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`server is listening on port ${port}...`);
 });
