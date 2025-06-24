620af117608283c55123fb5cd28d5e88187bfc73
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 15:05:27 +0800

    response html file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,10 @@ let app = express();
 
 console.log("Hello World");
 
+const indexPath = __dirname + "/views/index.html";
+
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(indexPath);
 });
 
 const port = process.env.PORT || 3000;
