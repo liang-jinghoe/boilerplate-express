f43486f0fc39fac1b3cdcb66497e35d7e87e48db
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 16:44:55 +0800

    middleware logger

--- myApp.js
+++ myApp.js
@@ -10,6 +10,16 @@ const indexPath = __dirname + "/views/index.html";
 
 app.use("/public", express.static(publicPath));
 
+app.use((req, res, next) => {
+    const method = req.method;
+    const path = req.path;
+    const ip = req.ip;
+
+    console.log(`${method} ${path} - ${ip}`);
+
+    next();
+});
+
 app.get("/json", (req, res) => {
     let message = "Hello json";
 
