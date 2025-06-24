037070e98d2962df29f2b2913934378f3cba1344
liangjinghoe <liang.jinghoe@1utar.my>
2025-06-24 16:17:51 +0800

    Fix static path

--- myApp.js
+++ myApp.js
@@ -6,17 +6,10 @@ console.log("Hello World");
 const publicPath = __dirname + "/public";
 const indexPath = __dirname + "/views/index.html";
 
-app.use(express.static(publicPath));
-
 app.use("/public", express.static(publicPath));
 
 app.get("/", (req, res) => {
     res.sendFile(indexPath);
-});
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`server is listening on port ${port}...`);
 });
 
 
